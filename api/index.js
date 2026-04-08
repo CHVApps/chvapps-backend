0ecdb536e614a35716faf41bc38e2bfe18760ea1
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2026-04-08 15:44:12 +0530

    vercel deployment issue fixed

--- api/index.js
+++ api/index.js
@@ -1,3 +1,50 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: 'CHV Apps Backend is running' });
-}
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors({
+  origin: 'https://chvapps.in',
+  credentials: true
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://chvapps.in');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'CHV Apps backend is running'
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'CHV Apps backend API is running'
+  });
+});
+
+app.get('/api/health', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Healthy'
+  });
+});
+
+module.exports = app;
