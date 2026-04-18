5553baa4a092d521951b35e715c94471539a06e6
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2026-04-18 13:33:23 +0530

    fix in the index page

--- api/index.js
+++ api/index.js
@@ -1,52 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const app = express();
-
-const allowedOrigins = [
-  'https://chvapps.in',
-  'https://www.chvapps.in',
-  'https://chvapps-admin.vercel.app',
-  'http://localhost:3000',
-  'http://localhost:3001'
-];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    return callback(new Error('Not allowed by CORS'));
-  },
-  credentials: true
-}));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'CHV Apps backend is running'
-  });
-});
-
-app.get('/api', (req, res) => {
-  res.status(200).json({
+export default function handler(req, res) {
+  return res.status(200).json({
     success: true,
     message: 'CHV Apps backend API is running'
   });
-});
-
-app.get('/api/health', (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'Healthy'
-  });
-});
-
-export default app;
+}
